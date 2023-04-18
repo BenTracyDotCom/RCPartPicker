d7f7ca2e5731642950607786967d500c80ad0ca1
Ben Tracy <b.rob.tracy@gmail.com>
2023-04-18 11:07:47 -0400

    implement login modal popup

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import React, { useState } from 'react'
 import './App.css'
 import Banner from './components/Banner'
-
+import LoginModal from './components/LoginModal'
 
 function App() {
+
+  const [user, setUser] = useState("")
+  const [builds, setBuilds] = useState([])
 
   return (
     <div className="App">
       <h1 className="text-red-600">RC Part Picker</h1>
-      <Banner />
+      <LoginModal setUser={setUser}/>
+      <Banner user={user} setUser={setUser} setBuilds={setBuilds} />
     </div>
   )
 }
--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,9 +1,24 @@
 import React from 'react';
 
-const Banner = (props:Object) => {
+const Banner = (props:{ user: String, setBuilds: Function, setUser: Function }) => {
+
+  const handleLogin = () => {
+    console.log(props.user, 'user', !!props.user)
+    if(!props.user){
+      const modalCheck = document?.getElementById('my-modal') as HTMLInputElement;
+      modalCheck.checked = true;
+    } else {
+      props.setBuilds([]);
+      props.setUser('');
+    }
+  }
 
   return (
-    <div>Banner</div>
+    <div>
+      <div>Banner</div>
+      <div>{props.user}</div>
+      <div onClick={handleLogin}>{!!props.user ? 'logout' : 'login'}</div>
+    </div>
   )
 }
 
