fd409414bd7523e9620ffbb78c6e7aca3983b5f3
Ben Tracy <b.rob.tracy@gmail.com>
2023-04-18 20:00:31 -0400

    add API routes to backend

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,8 +1,30 @@
-import axios from 'axios'
-const model =require('../../db/models')
+import axios from 'axios';
+import path from 'path';
 
 export default {
-  addPart: model.addPart,
-  getBuilds: model.getBuilds,
-  validateUser: model.validateUser
+  addPart: (formData: Object) => {
+    let payload = formData;
+    let options = {
+      method: 'POST',
+      url: path.join(__dirname, '/api/parts'),
+      data: payload
+    }
+    return axios(options)
+  },
+  getBuilds: (username: String) => {
+    let options = {
+      method: 'GET',
+      url: path.join(__dirname, `/api/builds/${username}`)
+    }
+    return axios(options)
+  },
+  validateUser: (userInfo:{email:String, password:String}) => {
+    let payload = userInfo
+    let options = {
+      method: 'POST',
+      url: path.join(__dirname, `/api/users`),
+      data: payload
+    }
+    return axios(options)
+  }
 }
